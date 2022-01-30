bce960c05d3360b0c928b3b683cdb0e239c325df
“Giulia <soccolinigiulia@gmail.com>
2022-01-30 12:53:22 +0100

    added search jokes

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Here the jokes</h1>
+
+    <SearchJokes v-on:search-text="searchText" />
+
     <Joke v-for="joke in jokes" :key="joke.id" :joke="joke.joke" :id="joke.id"/>
   </div>
 </template>
@@ -8,10 +11,24 @@
 <script>
 import axios from 'axios';
 import Joke from '../../components/Joke.vue';
+import SearchJokes from '../../components/SearchJokes.vue'
 
 export default {
+  head(){
+    return {
+      title: 'The Dad Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'About Nuxt Dad Jokes'
+        }
+      ]
+    }
+  },
   components: {
-    Joke
+    Joke,
+    SearchJokes
   },
   data(){
     return {
@@ -28,24 +45,26 @@ export default {
     try {
       const res = await axios.get('https://icanhazdadjoke.com/search', config);
 
-      // console.log(res.data);
-
       this.jokes = res.data.results;
-      console.log(this.jokes);
     } catch (err) {
       console.log(err)
     }
   },
-  head(){
-    return {
-      title: 'The Dad Jokes',
-      meta: [
-        {
-          hid: 'description',
-          name: 'description',
-          content: 'About Nuxt Dad Jokes'
+  methods:{
+    async searchText(text){
+      const config = {
+        headers: {
+          'Accept': 'application/json'
         }
-      ]
+      }
+
+      try {
+        const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config);
+
+        this.jokes = res.data.results;
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 }
