b735326583656a014ea9f1ff927e07936300e466
“Giulia <soccolinigiulia@gmail.com>
2022-01-30 11:55:05 +0100

    init

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>About Title</h1>
+    <p>page description</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style>
+
+</style>
